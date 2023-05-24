c38717db569b72e32bfcb737ddd74588d964b25c
Therese Linden <theresemlinden@gmail.com>
2023-05-24 15:08:48 +0200

    getting data and print to dom

--- client/src/pages/ProductList/ProductList.tsx
+++ client/src/pages/ProductList/ProductList.tsx
@@ -1,7 +1,22 @@
-type Props = {};
+import { useEffect } from 'react';
+import useFetch from '../../hooks/useFetch';
+//type Props = {};
 
-const ProductList = (props: Props) => {
-  return <div>ProductList</div>;
+const ProductList = () => {
+  const {
+    data: products,
+    isLoading,
+    errorMessage,
+  } = useFetch('/api/productss');
+
+  console.log(products, 'isLoading:', isLoading);
+  return (
+    <>
+      {errorMessage && <p>something went wrooooong</p>}
+      {isLoading && <p>Laddar........</p>}
+      {products && products.map(product => <h1>{product.title}</h1>)}
+    </>
+  );
 };
 
 export default ProductList;
