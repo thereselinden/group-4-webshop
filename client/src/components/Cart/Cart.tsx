d91ea1514e5972f2f690d95dc46f8491797782aa
Therese Linden <theresemlinden@gmail.com>
2023-05-24 15:07:06 +0200

    added icon

--- client/src/components/Cart/Cart.tsx
+++ client/src/components/Cart/Cart.tsx
@@ -1,7 +1,14 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import IconButton from '@mui/material/IconButton';
+
 type Props = {};
 
 const Cart = (props: Props) => {
-  return <div>Cart</div>;
+  return (
+    <IconButton aria-label="add to shopping cart">
+      <ShoppingCartIcon sx={{ color: 'white' }} />
+    </IconButton>
+  );
 };
 
 export default Cart;
