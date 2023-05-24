4bea61093966470e31bacc7d80ff01a84fc5ed8c
Therese Linden <theresemlinden@gmail.com>
2023-05-24 15:07:50 +0200

    render navitems

--- client/src/components/NavLinks/NavLinks.tsx
+++ client/src/components/NavLinks/NavLinks.tsx
@@ -1,7 +1,21 @@
-type Props = {};
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { NavItem } from '../../interfaces/interfaces';
 
-const NavLinks = (props: Props) => {
-  return <div>NavLinks</div>;
+type Props = {
+  navItems: NavItem[];
+};
+
+const NavLinks = ({ navItems }: Props) => {
+  return (
+    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+      {navItems.map(item => (
+        <Button key={item.name} sx={{ color: '#fff' }}>
+          {item.name}
+        </Button>
+      ))}
+    </Box>
+  );
 };
 
 export default NavLinks;
