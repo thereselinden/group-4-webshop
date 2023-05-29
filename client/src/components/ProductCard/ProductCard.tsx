9ba9a1352cd7062e9a2995a30213cc8f809dfb81
Therese Linden <theresemlinden@gmail.com>
2023-05-29 14:51:55 +0200

    Added Link wrapper

--- client/src/components/ProductCard/ProductCard.tsx
+++ client/src/components/ProductCard/ProductCard.tsx
@@ -8,35 +8,40 @@ import Typography from '@mui/material/Typography';
 import { IProduct } from '../../interfaces/interfaces';
 import AddToCartBtn from '../AddToCartBtn/AddToCartBtn';
 import { Box } from '@mui/system';
+import { CardActionArea } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 type Props = { product: IProduct };
 
 const ProductCard = ({ product }: Props) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component={'img'}
-        sx={{ height: 240 }}
-        image={product.image}
-        title={product.title}
-      />
-      <Box sx={{ p: 2 }}>
-        <Typography variant="h6" component="div">
-          {product.title}
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            mt: 1,
-          }}
-        >
-          <Typography variant="body2" color="text.secondary">
-            {product.price} kr
+      <Link to={product._id}>
+        <CardMedia
+          component={'img'}
+          sx={{ height: 240 }}
+          image={product.image}
+          title={product.title}
+        />
+        <Box sx={{ p: 2 }}>
+          <Typography variant="h6" component="div">
+            {product.title}
           </Typography>
-          <AddToCartBtn />
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              mt: 1,
+            }}
+          >
+            <Typography variant="body2" color="text.secondary">
+              {product.price} kr
+            </Typography>
+            <AddToCartBtn />
+          </Box>
         </Box>
-      </Box>
+      </Link>
     </Card>
   );
 };
