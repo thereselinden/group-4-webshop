592d4c95a700080912043cbcab7e8cefadb49df0
Therese Linden <theresemlinden@gmail.com>
2023-05-29 16:52:48 +0200

    Show number of cart items as number

--- client/src/components/Cart/Cart.tsx
+++ client/src/components/Cart/Cart.tsx
@@ -1,12 +1,22 @@
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import IconButton from '@mui/material/IconButton';
+import { useCartContext } from '../../context/CartContext';
 
 type Props = {};
 
 const Cart = (props: Props) => {
+  const { cartItems } = useCartContext();
+
+  let count = 0;
+
+  cartItems.forEach(item => {
+    count += item.quantity;
+  });
+
   return (
     <IconButton aria-label="add to shopping cart" color="textColor">
       <ShoppingCartIcon />
+      <p>{count}</p>
     </IconButton>
   );
 };
