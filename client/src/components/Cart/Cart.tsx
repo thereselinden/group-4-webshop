dd478a2993956830cfa84addd52c94bf5e52b37e
Therese Linden <theresemlinden@gmail.com>
2023-05-29 12:26:20 +0200

    Added icon btn and set color based on theme

--- client/src/components/Cart/Cart.tsx
+++ client/src/components/Cart/Cart.tsx
@@ -1,16 +1,11 @@
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import IconButton from '@mui/material/IconButton';
-import { theme } from '../../themes/themes';
 
 type Props = {};
 
 const Cart = (props: Props) => {
   return (
-    <IconButton
-      aria-label="add to shopping cart"
-      color="textColor"
-      // style={{ color: theme.palette.textColor.light }}
-    >
+    <IconButton aria-label="add to shopping cart" color="textColor">
       <ShoppingCartIcon />
     </IconButton>
   );
