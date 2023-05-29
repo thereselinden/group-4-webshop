5906de6efefb311a0d37ea97e09aedb9096e3764
Therese Linden <theresemlinden@gmail.com>
2023-05-29 10:11:52 +0200

    Wrapped with themeprovider
    Co-authored-by: johanbry <johanbry@users.noreply.github.com>
    Co-authored-by: Ammar Hasan <ammarhasan011@users.noreply.github.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+
+import { ThemeProvider } from '@emotion/react';
+
+import App from './App.tsx';
+import { theme } from './themes/themes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
