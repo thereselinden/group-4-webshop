70e709116c4026037a563854c942cabc8f5e5020
Therese Linden <theresemlinden@gmail.com>
2023-05-30 12:52:33 +0200

    imported numOfProducts and calcTotal interface created functions

--- client/src/context/CartContext.tsx
+++ client/src/context/CartContext.tsx
@@ -12,6 +12,8 @@ export const CartContext = createContext<ICartContext>({
   cartItems: [],
   addToCart: () => {},
   removeFromCart: () => {},
+  calcTotal: () => {},
+  numOfProducts: () => {},
 });
 
 export const useCartContext = () => useContext(CartContext);
@@ -19,8 +21,6 @@ export const useCartContext = () => useContext(CartContext);
 // Provider
 const CartProvider = ({ children }: PropsWithChildren) => {
   const [cartItems, setCartItems] = useState<ICartItem[]>([]);
-
-  console.log('cartItems', cartItems);
 
   //funktion som tar in en product i string
   const addToCart = (product: IProduct, quantity: number) => {
@@ -31,7 +31,6 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     );
 
     if (inCartIndex !== -1) {
-      console.log('inside if');
       const newItems = [...cartItems];
       newItems[inCartIndex].quantity += quantity;
       setCartItems(newItems);
@@ -41,8 +40,33 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     }
   };
 
-  const removeFromCart = () => {};
+  const removeFromCart = (id: string, qty = 1) => {
+    const inCartIndex = cartItems.findIndex(item => id === item.product._id);
 
+    if (inCartIndex !== -1) {
+      const newItems = [...cartItems];
+      if (newItems[inCartIndex].quantity === qty) {
+        newItems.splice(inCartIndex, 1);
+      } else {
+        newItems[inCartIndex].quantity -= qty;
+      }
+      setCartItems(newItems);
+    }
+  };
+
+  const calcTotal = (): number => {
+    let sum = 0;
+    cartItems.forEach(item => (sum += item.quantity * item.product.price));
+
+    return sum;
+  };
+
+  const numOfProducts = (): number => {
+    let prodqty = 0;
+    cartItems.forEach(item => (prodqty += item.quantity));
+
+    return prodqty;
+  };
   // Om den finns uppdatera qty
 
   // Om den inte finns, lägg till hela objektet (produkt)
@@ -51,7 +75,9 @@ const CartProvider = ({ children }: PropsWithChildren) => {
   //emmellan Contexten lägger vi (props.)children
   //Proivdea med value ut det vi vill göra synligt. Value måste matcha Interface. Eftersom vi typat upp context så. Därför value propen. {{}} pga gör det som ett objekt
   return (
-    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+    <CartContext.Provider
+      value={{ cartItems, addToCart, removeFromCart, calcTotal, numOfProducts }}
+    >
       {children}
     </CartContext.Provider>
   );
