a70b2f9e841f59d4ed2a566b0dbcffcafcc7c382
Therese Linden <theresemlinden@gmail.com>
2023-05-30 12:51:56 +0200

    added badge and get total products from context

--- client/src/components/Cart/Cart.tsx
+++ client/src/components/Cart/Cart.tsx
@@ -1,23 +1,37 @@
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import IconButton from '@mui/material/IconButton';
 import { useCartContext } from '../../context/CartContext';
+import { useState } from 'react';
+import CartDrawer from '../CartDrawer/CartDrawer';
+import Badge from '@mui/material/Badge';
+import { theme } from '../../themes/themes';
 
 type Props = {};
 
 const Cart = (props: Props) => {
-  const { cartItems } = useCartContext();
-
-  let count = 0;
-
-  cartItems.forEach(item => {
-    count += item.quantity;
-  });
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const { numOfProducts } = useCartContext();
 
   return (
-    <IconButton aria-label="add to shopping cart" color="textColor">
-      <ShoppingCartIcon />
-      <p>{count}</p>
-    </IconButton>
+    <>
+      <IconButton
+        aria-label="add to shopping cart"
+        color="textColor"
+        onClick={() => setDrawerOpen(true)}
+      >
+        <Badge
+          badgeContent={numOfProducts()}
+          color="textColor1"
+          // sx={{
+          //   backgroundColor: theme.palette.accent.background,
+          //   color: theme.palette.accent.color,
+          // }}
+        >
+          <ShoppingCartIcon color="action" />
+        </Badge>
+      </IconButton>
+      <CartDrawer drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} />
+    </>
   );
 };
 
