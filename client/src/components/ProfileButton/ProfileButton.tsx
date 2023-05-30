0642828b85193c8b87fe1655c77845db368c8f23
Therese Linden <theresemlinden@gmail.com>
2023-05-30 16:35:42 +0200

    Trigger modal open logic

--- client/src/components/ProfileButton/ProfileButton.tsx
+++ client/src/components/ProfileButton/ProfileButton.tsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
+
 import LoginIcon from '@mui/icons-material/Login';
 import IconButton from '@mui/material/IconButton';
+import UserModal from '../UserModal/UserModal';
 
 type Props = {};
 
 const ProfileButton = (props: Props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+
+  const handleClose = () => setOpen(false);
+
   return (
-    <IconButton aria-label="add to shopping cart">
-      <LoginIcon color="textColor" />
-    </IconButton>
+    <>
+      <IconButton aria-label="add to shopping cart" onClick={handleOpen}>
+        <LoginIcon color="textColor" />
+      </IconButton>
+      <UserModal handleClose={handleClose} open={open} />
+    </>
   );
 };
 
