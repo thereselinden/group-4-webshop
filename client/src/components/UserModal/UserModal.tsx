5187e84b1b021aacd37da6872250e12dbc7ef1e5
Therese Linden <theresemlinden@gmail.com>
2023-05-30 16:35:57 +0200

    Created modal

---
+++ client/src/components/UserModal/UserModal.tsx
@@ -0,0 +1,52 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import LoginForm from '../Form/LoginForm';
+import { useState } from 'react';
+import RegisterForm from '../Form/RegisterForm';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+type Props = {
+  open: boolean;
+  handleClose: () => void;
+};
+
+const UserModal = ({ open, handleClose }: Props) => {
+  const [hasAccount, setHasAccount] = useState(true);
+
+  const handleAccount = () => {
+    setHasAccount(!hasAccount);
+  };
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={() => handleClose()}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          {hasAccount ? (
+            <LoginForm handleAccount={handleAccount} />
+          ) : (
+            <RegisterForm handleAccount={handleAccount} />
+          )}
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default UserModal;
