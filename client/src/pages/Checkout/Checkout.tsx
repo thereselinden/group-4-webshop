c43e64e8de679273978b95a18e496f0d5ad0c357
Therese Linden <theresemlinden@gmail.com>
2023-06-01 16:21:20 +0200

    Checkup settings

--- client/src/pages/Checkout/Checkout.tsx
+++ client/src/pages/Checkout/Checkout.tsx
@@ -1,7 +1,30 @@
+import CartItemList from '../../components/CartItemList/CartItemList';
+
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+import Typography from '@mui/material/Typography';
+import Shipping from '../../components/Shipping/Shipping';
+
 type Props = {};
 
 const Checkout = (props: Props) => {
-  return <div>Checkout</div>;
+  return (
+    <>
+      <h1>Checkout</h1>
+
+      <Box>
+        <Box>
+          <Shipping />
+          <Paper variant="outlined">
+            <Typography variant="h3">Dina uppgifter</Typography>
+          </Paper>
+        </Box>
+
+        <CartItemList />
+      </Box>
+    </>
+  );
 };
 
 export default Checkout;
