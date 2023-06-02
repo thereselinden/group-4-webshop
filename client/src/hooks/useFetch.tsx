ddaf7087f05cf3b5f5632dc5d012085da54bfd2d
Therese Linden <theresemlinden@gmail.com>
2023-06-02 15:15:20 +0200

    refactor code

--- client/src/hooks/useFetch.tsx
+++ client/src/hooks/useFetch.tsx
@@ -1,6 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 
-const useFetch = <T,>(url: string) => {
+const useFetch = <T,>(
+  url: string,
+  method = 'GET',
+  body?: string
+): [
+  [T | null, Dispatch<SetStateAction<T | null>>],
+  [boolean, Dispatch<SetStateAction<boolean>>],
+  [string | null, Dispatch<SetStateAction<string | null>>]
+] => {
   const [data, setData] = useState<T | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
@@ -10,7 +18,11 @@ const useFetch = <T,>(url: string) => {
       setIsLoading(true);
 
       try {
-        const response = await fetch(url);
+        const response = await fetch(url, {
+          method: method,
+          headers: { 'Content-Type': 'application/json' },
+          body: body,
+        });
         const data = await response.json();
         if (response.ok) {
           setData(data);
@@ -27,7 +39,12 @@ const useFetch = <T,>(url: string) => {
     fetchData();
   }, [url]);
 
-  return { data, isLoading, errorMessage };
+  // return { data, isLoading, errorMessage };
+  return [
+    [data, setData],
+    [isLoading, setIsLoading],
+    [errorMessage, setErrorMessage],
+  ];
 };
 
 export default useFetch;
