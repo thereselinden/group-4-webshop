819cfcb4d6316f1c7f19d71cf4ee13a435899c57
Therese Linden <theresemlinden@gmail.com>
2023-06-02 15:16:29 +0200

    Generics or null as return value. Check response if data returned

--- client/src/utils/FetchData.ts
+++ client/src/utils/FetchData.ts
@@ -2,7 +2,7 @@ const fetchData = async <T>(
   url: string,
   method = 'GET',
   body?: any
-): Promise<T> => {
+): Promise<T | null> => {
   console.log('fetch data body', body);
   try {
     const response = await fetch(url, {
@@ -11,9 +11,11 @@ const fetchData = async <T>(
       body: body,
     });
     if (!response.ok) throw new Error(response.status.toString());
-    console.log('fetch data response', response);
 
+    //! KAN VI KOLLA DETTA PÅ ANNAT SÄTT?
+    if (response.statusText === 'No Content') return null;
     const data = await response.json();
+
     return data;
   } catch (error) {
     throw error as Error;
