3add8d2bd18a6b92dc345a0f5bb031a616c0015e
Therese Linden <theresemlinden@gmail.com>
2023-06-04 21:31:38 +0200

    Helper function return formatted delivery time

---
+++ client/src/utils/DeliveryDate.ts
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs';
+import 'dayjs/locale/sv';
+
+export const deliveryDate = (hours: number) => {
+  //const todaysDate = new Date();
+
+  const currentHour = dayjs().hour();
+  if (currentHour > 17) hours += 24;
+  return dayjs().locale('sv').add(hours, 'hour').format('D MMMM');
+};
