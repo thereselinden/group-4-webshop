3ff5abf78e6776eb752a8a2ced1f23f4336dc9b3
Therese Linden <theresemlinden@gmail.com>
2023-06-04 21:32:01 +0200

    Basic styling

--- client/src/components/Shipping/Shipping.tsx
+++ client/src/components/Shipping/Shipping.tsx
@@ -1,3 +1,4 @@
+import { styled } from '@mui/system';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -5,12 +6,19 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-
 import Typography from '@mui/material/Typography';
 
 import useFetch from '../../hooks/useFetch';
 import { IShipping } from '../../interfaces/interfaces';
+import { deliveryDate } from '../../utils/DeliveryDate';
+
 type Props = {};
+
+const CheckoutOption = styled(Box)({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
 
 const Shipping = (props: Props) => {
   const [
@@ -18,33 +26,35 @@ const Shipping = (props: Props) => {
     [isLoading, setIsLoading],
     [errorMessage, setErrorMessage],
   ] = useFetch<IShipping[]>('/api/shippingmethod');
-  console.log(shippingmethods);
 
   return (
-    <>
-      <Paper variant="outlined">
-        <Typography variant="h3">Leveransmetod</Typography>
-        <FormControl>
-          <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
-          <RadioGroup
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="female"
-            name="radio-buttons-group"
-          >
-            {shippingmethods?.map(method => (
-              <>
-                <FormControlLabel
-                  value={method.company}
-                  control={<Radio />}
-                  label={method.company}
-                />
-                <Typography>{method.price} kr</Typography>
-              </>
-            ))}
-          </RadioGroup>
-        </FormControl>
-      </Paper>
-    </>
+    <Paper variant="outlined" sx={{ mb: 4 }}>
+      <Typography variant="h4" component="h2">
+        Leveransmetod
+      </Typography>
+      <FormControl sx={{ width: '100%', p: 3 }}>
+        <FormLabel id="demo-radio-buttons-group-label">Leveransval</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+        >
+          {shippingmethods?.map(method => (
+            <CheckoutOption key={method.company}>
+              <FormControlLabel
+                value={method.company}
+                control={<Radio />}
+                label={method.company}
+              />
+              <Typography variant="subtitle1" component="p">
+                Levereras: {deliveryDate(method.deliveryTimeInHours)}
+              </Typography>
+              <Typography>{method.price} SEK</Typography>
+            </CheckoutOption>
+          ))}
+        </RadioGroup>
+      </FormControl>
+    </Paper>
   );
 };
 
--- client/src/pages/Checkout/Checkout.tsx
+++ client/src/pages/Checkout/Checkout.tsx
@@ -1,29 +1,32 @@
 import CartItemList from '../../components/CartItemList/CartItemList';
+import Shipping from '../../components/Shipping/Shipping';
 
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-
 import Typography from '@mui/material/Typography';
-import Shipping from '../../components/Shipping/Shipping';
+import { styled } from '@mui/material';
 
 type Props = {};
 
+const CheckoutContainer = styled(Box)({
+  display: 'grid',
+  gridTemplateColumns: '3fr auto',
+});
+
 const Checkout = (props: Props) => {
   return (
-    <>
-      <h1>Checkout</h1>
-
+    <CheckoutContainer>
       <Box>
-        <Box>
-          <Shipping />
-          <Paper variant="outlined">
-            <Typography variant="h3">Dina uppgifter</Typography>
-          </Paper>
-        </Box>
-
-        <CartItemList />
+        <Shipping />
+        <Paper variant="outlined">
+          <Typography variant="h4" component="h2">
+            Dina uppgifter
+          </Typography>
+        </Paper>
       </Box>
-    </>
+
+      <CartItemList />
+    </CheckoutContainer>
   );
 };
 
