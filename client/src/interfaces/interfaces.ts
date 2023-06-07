6113faf68c1d5a4024f06b5435fb995b35902874
Therese Linden <theresemlinden@gmail.com>
2023-06-07 12:58:37 +0200

    Added key value pair to ICartContext created interfaces for orderItem and confirmed order

--- client/src/interfaces/interfaces.ts
+++ client/src/interfaces/interfaces.ts
@@ -22,6 +22,7 @@ export interface ICartContext {
   cartItems: ICartItem[];
   addToCart: (product: IProduct, quantity: number) => void;
   removeFromCart: (id: string, qty?: number) => void;
+  clearCart: () => void;
   calcProductTotal: () => number;
   numOfProducts: () => number;
 }
@@ -44,6 +45,7 @@ export interface IUser {
   lastName: string;
   email: string;
   isAdmin: boolean;
+  __v?: number;
 }
 
 export interface ILoginResponse {
@@ -64,25 +66,33 @@ export interface IUserContext {
   logout: () => void;
 }
 export interface IShipping {
+  _id: string;
   company: string;
   price: number;
   deliveryTimeInHours: number;
+  __v?: number;
 }
 
-// export interface IOrder {
-//   orderNumber: number,
-//   customer: IUser,
-//   deliveryAddress: {
-//     street: string,
-//     zip: number,
-//     city: string,
-//     country: string,
-//   }
-//   orderItems: {
-//     item: ICartItem,
-//     price: number
-//   }
-// }
-// export interface IOrderContext {
-//   orders: IOrder[]
-// }
+export interface IDeliveryAddress {
+  street: string;
+  zipcode: string;
+  city: string;
+  country: string;
+}
+
+export interface IOrderItem extends ICartItem {
+  price: number;
+}
+
+export interface IConfirmedOrder {
+  createdAt: string;
+  customer: IUser;
+  deliveryAddress: IDeliveryAddress;
+  orderItems: IOrderItem[];
+  orderNumber: string;
+  shipped: boolean;
+  shippingMethod: IShipping;
+  updatedAt: string;
+  __v: number;
+  _id: string;
+}
