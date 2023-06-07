1f5df8dd31085931c78e61b9a3fa5a95a10ce580
Therese Linden <theresemlinden@gmail.com>
2023-06-07 19:01:58 +0200

    Remove map to render menu links. Added routing

--- client/src/pages/UserProfile/UserProfile.tsx
+++ client/src/pages/UserProfile/UserProfile.tsx
@@ -10,14 +10,22 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import Typography from '@mui/material/Typography';
 import ListItemIcon from '@mui/material/ListItemIcon';
+import { IConfirmedOrder } from '../../interfaces/interfaces';
+import { Route, Routes, Link } from 'react-router-dom';
+import OverView from './OverView';
+import MyOrders from './MyOrders';
 // import { ListItemIcon } from '@mui/material';
 
 type Props = {};
 
 const navItems = ['Mina köp', 'Logga ut'];
+
+// ordrar produkter
+
 //const handleLogout =()=>{logout()};
 const UserProfile = (props: Props) => {
   const { user, logout } = useUserContext();
+  const [orders, setOrders] = useState<IConfirmedOrder[]>(null);
 
   return (
     <>
@@ -27,26 +35,58 @@ const UserProfile = (props: Props) => {
             <Typography variant="h5" component="p">
               Mina sidor
             </Typography>
-            {navItems.map((item, index) => (
-              <ListItem key={index} disablePadding>
+            <Link to="profile/my-orders">
+              <ListItem disablePadding>
                 <ListItemButton>
                   <ListItemIcon>
-                    {index % 2 === 0 ? (
-                      <ShoppingBasketIcon />
-                    ) : (
-                      <LogoutIcon onClick={() => logout()} />
-                    )}
+                    <ShoppingBasketIcon />
                   </ListItemIcon>
-                  <ListItemText primary={item} />
+                  <ListItemText primary={'Mina köp'} />
                 </ListItemButton>
               </ListItem>
-            ))}
+            </Link>
+            {user?.isAdmin && (
+              <>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <ShoppingBasketIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={'Alla ordrar'} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <ShoppingBasketIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={'Alla produkter'} />
+                  </ListItemButton>
+                </ListItem>
+              </>
+            )}
+
+            <ListItem disablePadding onClick={() => logout()}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <LogoutIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Logga ut'} />
+              </ListItemButton>
+            </ListItem>
           </List>
         </Paper>
+
         <Box component="div" sx={{ flexGrow: 1, p: 3 }}>
-          <Typography variant="h4" component="h1">
+          <Routes>
+            <Route path="/profile/overview" element={<OverView />} />
+            <Route path="/profile/my-orders" element={<MyOrders />} />
+            <Route path="/profile/admin-orders" element={<OverView />} />
+            <Route path="/profile/admin-products" element={<OverView />} />
+          </Routes>
+          {/* <Typography variant="h4" component="h1">
             Hej {user?.firstName}
-          </Typography>
+          </Typography> */}
         </Box>
       </Box>
     </>
