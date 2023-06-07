84e1123ca60a088e8154f8f007bb63ea75f71e66
Therese Linden <theresemlinden@gmail.com>
2023-06-07 13:00:16 +0200

    Logic to format placed order info, calc ordertota

--- client/src/utils/helper.ts
+++ client/src/utils/helper.ts
@@ -1,12 +1,17 @@
-import dayjs from "dayjs";
-import "dayjs/locale/sv";
+import dayjs from 'dayjs';
+import 'dayjs/locale/sv';
+import { IOrderItem } from '../interfaces/interfaces';
 
 export const deliveryDate = (hours: number) => {
   //const todaysDate = new Date();
 
   const currentHour = dayjs().hour();
   if (currentHour > 17) hours += 24;
-  return dayjs().locale("sv").add(hours, "hour").format("D MMMM");
+  return dayjs().locale('sv').add(hours, 'hour').format('D MMMM');
+};
+
+export const formatOrderDate = (date: string) => {
+  return dayjs(date).locale('sv').format('D MMMM HH:mm');
 };
 
 export const calcOrderTotal = (
@@ -14,4 +19,17 @@ export const calcOrderTotal = (
   shippingPrice: number
 ) => {
   return productPrice() + shippingPrice;
+};
+
+export const calcOrderItemTotal = (qty: number, price: number): number => {
+  return qty * price;
+};
+
+export const calcOrderProductTotal = (orderItems: IOrderItem[]): number => {
+  const initialValue = 0;
+  return orderItems.reduce(
+    (accumulator, item) =>
+      accumulator + calcOrderItemTotal(item.quantity, item.price),
+    initialValue
+  );
 };
