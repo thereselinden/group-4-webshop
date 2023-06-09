8f0f599ea4166b74b2a4d218e3205888206b1022
Therese Linden <theresemlinden@gmail.com>
2023-06-09 16:20:23 +0200

    Product provider. Logic to get all get on and delete one product

---
+++ client/src/context/ProductContext.tsx
@@ -0,0 +1,74 @@
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+import { ICategory, IProduct, IProductContext } from '../interfaces/interfaces';
+import fetchData from '../utils/FetchData';
+
+export const ProductContext = createContext<IProductContext>(null as any);
+
+export const useProductContext = () => useContext(ProductContext);
+
+const ProductProvider = ({ children }: PropsWithChildren) => {
+  const [products, setProducts] = useState<IProduct[] | null>(null);
+  const [categories, setCategories] = useState<ICategory[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    getProducts();
+    getCategories();
+  }, []);
+
+  const getProducts = async (): Promise<void> => {
+    setIsLoading(true);
+    try {
+      const products = await fetchData<IProduct[]>('/api/products');
+      setProducts(products);
+      setIsLoading(false);
+    } catch (error) {
+      setErrorMessage((error as Error).message);
+      setIsLoading(false);
+    }
+  };
+
+  const getCategories = async (): Promise<void> => {
+    setIsLoading(true);
+    try {
+      const categories = await fetchData<ICategory[]>('/api/categories');
+      setCategories(categories);
+    } catch (error) {
+      setErrorMessage((error as Error).message);
+      setIsLoading(false);
+    }
+  };
+
+  const getProduct = (id: string): IProduct | void => {
+    const product = products?.find(prod => prod._id === id);
+    if (product) return product;
+    return;
+  };
+
+  const deleteProduct = async (id: string): Promise<void> => {
+    try {
+      await fetchData(`/api/products/${id}`, 'DELETE');
+      getProducts();
+    } catch (error) {
+      setErrorMessage((error as Error).message);
+    }
+  };
+
+  return (
+    <ProductContext.Provider
+      value={{ products, categories, isLoading, getProduct, deleteProduct }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductProvider;
