ab7a342e1aecb2531f0e468efb22098b048e207f
Therese Linden <theresemlinden@gmail.com>
2023-06-13 12:20:56 +0200

    Render different hero img depending on screen size
    Co-authored-by: johanbry <johanbry@users.noreply.github.com>
    Co-authored-by: Nikela <nikelailona@gmail.com>
    Co-authored-by: Ammar Hasan <ammarhasan011@users.noreply.github.com>

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,7 +1,14 @@
-type Props = {};
+import HeroDesktop from '../../assets/hero.png';
+import HeroMobile from '../../assets/hero-mobile.png';
 
-const Hero = (props: Props) => {
-  return <div>Hero</div>;
+const Hero = () => {
+  return (
+    <picture>
+      <source media="(max-width: 668px)" srcSet={HeroMobile} />
+      <source media="(max-width: 1200px)" srcSet={HeroDesktop} />
+      <img src={HeroDesktop} alt="Store hero image" style={{ width: '100%' }} />
+    </picture>
+  );
 };
 
 export default Hero;
