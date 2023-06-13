0cedd44fbc8fbadfa8eaec6b6c225733101da90e
Therese Linden <theresemlinden@gmail.com>
2023-06-13 08:31:40 +0200

    Text depending on whos logged in

--- client/src/pages/UserProfile/OverView.tsx
+++ client/src/pages/UserProfile/OverView.tsx
@@ -5,9 +5,21 @@ const Overview = () => {
   const { user } = useUserContext();
 
   return (
-    <Typography variant="h4" component="h1">
-      Hej {user?.firstName}
-    </Typography>
+    <>
+      <Typography variant="h4" component="h1">
+        Hej {user?.firstName}
+      </Typography>
+      {user?.isAdmin ? (
+        <Typography>
+          Använd menyn till vänster för att se dina köp, hantera ordrar,
+          uppdatera eller lägg till produkt!
+        </Typography>
+      ) : (
+        <Typography>
+          Använd menyn till vänster för att se alla dina köp!
+        </Typography>
+      )}
+    </>
   );
 };
 
