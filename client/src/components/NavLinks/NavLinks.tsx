6f5ba4cdf6a3a366bbdf2e8857d7f4fc2061a8bd
Therese Linden <theresemlinden@gmail.com>
2023-06-13 09:25:58 +0200

    Changed navItems to be dynamic

--- client/src/components/NavLinks/NavLinks.tsx
+++ client/src/components/NavLinks/NavLinks.tsx
@@ -1,18 +1,19 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
-import { NavItem } from '../../interfaces/interfaces';
+import { ICategory, NavItem } from '../../interfaces/interfaces';
 
 type Props = {
-  navItems: NavItem[];
+  //navItems: NavItem[];
+  navItems: ICategory[] | null;
 };
 
 const NavLinks = ({ navItems }: Props) => {
   return (
-    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-      {navItems.map(item => (
-        <Button key={item.name} color="textColor">
-          {item.name}
+    <Box sx={{ display: { xs: 'none', sm: 'block' }, ml: 3 }}>
+      {navItems?.map(item => (
+        <Button key={item.title} color="textColor">
+          {item.title}
         </Button>
       ))}
     </Box>
