ba826352e727711ce0ed349990a711c0f84af995
Therese Linden <theresemlinden@gmail.com>
2023-06-13 12:22:07 +0200

    Added Hero img and heading

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,3 +1,6 @@
+import Typography from '@mui/material/Typography';
+
+import Hero from '../../components/Hero/Hero';
 import ProductList from '../../components/ProductList/ProductList';
 import { useProductContext } from '../../context/ProductContext';
 
@@ -5,7 +8,15 @@ type Props = {};
 
 const Home = (props: Props) => {
   const { products } = useProductContext();
-  return <>{products && <ProductList products={products} />}</>;
+  return (
+    <>
+      <Hero />
+      <Typography variant="h4" component="h2" sx={{ mt: 3 }}>
+        Alla produkter
+      </Typography>
+      {products && <ProductList products={products} />}
+    </>
+  );
 };
 
 export default Home;
