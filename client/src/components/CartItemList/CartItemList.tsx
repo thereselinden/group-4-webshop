130b3e6eb7da25e392402a6b216bcaf2d82ed9bf
Therese Linden <theresemlinden@gmail.com>
2023-06-14 19:08:10 +0200

    render only if cartitems is true

--- client/src/components/CartItemList/CartItemList.tsx
+++ client/src/components/CartItemList/CartItemList.tsx
@@ -13,53 +13,54 @@ const CartItemList = () => {
 
   return (
     <Box>
-      {cartItems.map(item => (
-        <ListItem
-          sx={{
-            display: 'grid',
-            justifyContent: 'space-between',
-            gridTemplateColumns: '1fr 2fr auto',
-            gap: 1,
-          }}
-          key={item.product._id}
-        >
-          <Box
-            component="img"
-            src={item.product.image}
-            alt={item.product.title}
-            sx={{ width: 75 }}
-          />
-          <Box>
-            <Typography
-              variant="subtitle2"
-              component="p"
-              style={{ marginLeft: 8, fontWeight: 700 }}
-            >
-              {item.product.title}
-            </Typography>
-            <Typography style={{ marginLeft: 8 }}>
-              {item.product.price} kr
-            </Typography>
-            <Typography>
-              <IconButton onClick={() => removeFromCart(item.product._id)}>
-                <RemoveIcon sx={{ fontSize: 15 }} />
-              </IconButton>
+      {cartItems &&
+        cartItems.map(item => (
+          <ListItem
+            sx={{
+              display: 'grid',
+              justifyContent: 'space-between',
+              gridTemplateColumns: '1fr 2fr auto',
+              gap: 1,
+            }}
+            key={item.product._id}
+          >
+            <Box
+              component="img"
+              src={item.product.image}
+              alt={item.product.title}
+              sx={{ width: 75 }}
+            />
+            <Box>
+              <Typography
+                variant="subtitle2"
+                component="p"
+                style={{ marginLeft: 8, fontWeight: 700 }}
+              >
+                {item.product.title}
+              </Typography>
+              <Typography style={{ marginLeft: 8 }}>
+                {item.product.price} kr
+              </Typography>
+              <Typography>
+                <IconButton onClick={() => removeFromCart(item.product._id)}>
+                  <RemoveIcon sx={{ fontSize: 15 }} />
+                </IconButton>
 
-              {item.quantity}
-              <IconButton onClick={() => addToCart(item.product, 1)}>
-                <AddIcon sx={{ fontSize: 15 }} />
+                {item.quantity}
+                <IconButton onClick={() => addToCart(item.product, 1)}>
+                  <AddIcon sx={{ fontSize: 15 }} />
+                </IconButton>
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton
+                onClick={() => removeFromCart(item.product._id, item.quantity)}
+              >
+                <DeleteOutlineIcon />
               </IconButton>
-            </Typography>
-          </Box>
-          <Box>
-            <IconButton
-              onClick={() => removeFromCart(item.product._id, item.quantity)}
-            >
-              <DeleteOutlineIcon />
-            </IconButton>
-          </Box>
-        </ListItem>
-      ))}
+            </Box>
+          </ListItem>
+        ))}
     </Box>
   );
 };
