d1fb510ad5298ad11380247876b3dd8aeaab049f
Therese Linden <theresemlinden@gmail.com>
2023-06-15 14:43:44 +0200

    translated joi error message

--- client/src/components/Form/formValidate.ts
+++ client/src/components/Form/formValidate.ts
@@ -1,33 +1,48 @@
 import Joi from 'joi';
 
+const language = {
+  'string.empty': `Kan inte vara ett tomt fält`,
+  'string.email': `Ogiltig e-postadress`,
+  'string.min': `Måste vara minst {#limit} tecken långt`,
+  'string.uri': 'Måste vara en URI adress',
+  'number.base': 'Måste vara ett nummer',
+  'any.required': `Fältet är obligatoriskt`,
+};
+
 export const loginSchema = Joi.object({
   email: Joi.string()
     .email({ tlds: { allow: false } })
-    .required(),
-  password: Joi.string().min(4).required(),
+    .required()
+    .messages(language),
+  password: Joi.string().min(4).required().messages(language),
 });
 
 export const registerSchema = Joi.object({
-  firstName: Joi.string().min(2).required(),
-  lastName: Joi.string().min(4).required(),
+  firstName: Joi.string().min(2).required().messages(language),
+  lastName: Joi.string().min(4).required().messages(language),
   email: Joi.string()
     .email({ tlds: { allow: false } })
-    .required(),
-  password: Joi.string().min(4).required(),
+    .required()
+    .messages(language),
+  password: Joi.string().min(4).required().messages(language),
 });
 
 export const deliveryAddressSchema = Joi.object({
-  street: Joi.string().strict().required(),
-  zipcode: Joi.string().strict().min(5).required(),
-  city: Joi.string().strict().required(),
-  country: Joi.string().strict().required(),
+  street: Joi.string().strict().required().messages(language),
+  zipcode: Joi.string().strict().min(5).required().messages(language),
+  city: Joi.string().strict().required().messages(language),
+  country: Joi.string().strict().required().messages(language),
 });
 
 export const productSchema = Joi.object({
-  title: Joi.string().strict().required(),
-  description: Joi.string().strict().required(),
-  price: Joi.number().required(),
-  image: Joi.string().uri().allow('image/png', 'image/jpeg').required(),
-  inStock: Joi.number().required(),
-  categories: Joi.array().min(1),
+  title: Joi.string().strict().required().messages(language),
+  description: Joi.string().strict().required().messages(language),
+  price: Joi.number().required().messages(language),
+  image: Joi.string()
+    .uri()
+    .allow('image/png', 'image/jpeg')
+    .required()
+    .messages(language),
+  inStock: Joi.number().required().messages(language),
+  categories: Joi.array().min(1).messages(language),
 });
